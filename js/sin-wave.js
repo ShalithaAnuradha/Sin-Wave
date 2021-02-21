840edf71d2e4c43549aef1b0be944b8e64d5f184
shalitha <shalithaanuradha123@gmail.com>
2021-02-21 17:26:57 +0530

    Create simple harmonic motion for an element

---
+++ js/sin-wave.js
@@ -0,0 +1,42 @@
+// document.body.innerText='sfffsfffsdfdfsdfsdfdffsdfsdfdfdfsdfdfsdfsfsfffsdfsdfsdfsfssfffsdf';
+
+let boxElement = document.getElementById("box");
+
+// boxElement.style.top='100px';
+
+var step=0;
+var down=true;
+var up=false;
+var x_direction=0;
+function Sin(){
+    if(up==false){
+        step+=1;
+        boxElement.style.top=500+step+'px';
+        boxElement.style.left=x_direction+'px';
+    }
+    else{
+        step-=1;
+        boxElement.style.top=500+step+'px';
+        boxElement.style.left=x_direction+'px';
+    }
+    if(boxElement.style.top>="700px" ){
+        // console.log("Test1")
+        down=false;
+        up=true;
+        // x_direction+=50;
+        // boxElement.style.left=x_direction+'px';
+    }
+    if(boxElement.style.top<='200px'){
+        // console.log("Test2")
+        down=true;
+        up=false;
+        // x_direction+=50;
+        // boxElement.style.left=x_direction+'px';
+    }
+}
+
+
+for (let i = 0; i <10 ; i++) {
+    setInterval(Sin,i*3000);
+}
+
